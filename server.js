5813e2fddbe0400f860a72bd8b1c9cbddc940c65
lindsfitz <lindsaytfitz@gmail.com>
2021-11-16 15:53:29 -0800

    deploying

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,});
 
 app.get('/exercise', (req,res) => {
   res.sendFile(path.join(__dirname,'public/exercise.html'))
