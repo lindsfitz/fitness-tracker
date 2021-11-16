bd47d1c5c06400c1e2cb2e3f2beb4199df93bf76
lindsfitz <lindsaytfitz@gmail.com>
2021-11-16 15:33:07 -0800

    fix exercises get route

--- server.js
+++ server.js
@@ -27,7 +27,16 @@ app.get('/stats', (req,res) => {
 })
 
 app.get('/api/workouts', (req,res) => {
-  db.Workout.find({})
+  // db.Workout.find({})
+  db.Workout.aggregate([
+    
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: '$exercises.duration'}}
+    }
+
+  ])
   .then(dbWorkout => {
     res.json(dbWorkout)
   })
