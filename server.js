0f3f7a7a5a58d25cb08c463136484f3e972861b8
lindsfitz <lindsaytfitz@gmail.com>
2021-11-16 15:25:26 -0800

    routes complete

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
-const Workout = require("./workoutModel.js");
+const db = require("./models")
+const Workout = require("./models/Workout.js");
 const app = express();
 
 app.use(logger("dev"));
@@ -14,7 +16,69 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.get('/exercise', (req,res) => {
+  res.sendFile(path.join(__dirname,'public/exercise.html'))
+})
+
+app.get('/stats', (req,res) => {
+  res.sendFile(path.join(__dirname,'public/stats.html'))
+})
+
+app.get('/api/workouts', (req,res) => {
+  db.Workout.find({})
+  .then(dbWorkout => {
+    res.json(dbWorkout)
+  })
+  .catch(err => {
+    res.json(err)
+  })
+})
+
+app.post('/api/workouts', ({ body },res) => {
+  const exercise = new Workout(body);
+  db.Workout.create(exercise)
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.json(err);
+  })
+
+})
+
+app.put('/api/workouts/:id',(req,res) => {
+  db.Workout.findByIdAndUpdate(req.params.id,{$push:{exercises:req.body}},{new: true})
+  .then(dbWorkout => {
+    res.json(dbWorkout)
+  })
+  .catch(err => {
+    res.json(err)
+  })
+
+})
+
+app.get('/api/workouts/range', (req,res) => {
+  db.Workout.aggregate([
+    
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: '$exercises.duration'}}
+    }
+
+  ])
+  .sort({_id: -1})
+  .limit(7)
+  .then(dbWorkout => {
+    res.json(dbWorkout)
+  })
+  .catch(err => {
+    res.json(err)
+  })
+
+})
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
